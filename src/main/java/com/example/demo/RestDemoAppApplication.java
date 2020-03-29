e9bc2217f293ad764d1880bac86512b6f16a1786
slims786 <slims778866@gmail.com>
2020-03-29 16:20:26 +0530

    First commit

---
+++ src/main/java/com/example/demo/RestDemoAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoAppApplication.class, args);
+		System.out.println("Main class");
+	}
+
+}
